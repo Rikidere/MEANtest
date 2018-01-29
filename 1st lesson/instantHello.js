1cbe20aa80742a5f88e7b5e91590d5eb1cbb6ce5
Rikidere <rorikidesu@gmail.com>
2018-01-29 19:35:39 +0100

    first commit

---
+++ 1st lesson/instantHello.js
@@ -0,0 +1,2 @@
+var name = "Riki";
+console.log("Hello " + name);
---
+++ 2nd lesson (async)/setTimeout.js
@@ -0,0 +1,7 @@
+console.log("1: Start App");
+
+var holdOn = setTimeout(function() {
+  console.log("2: in the setTimeout");
+}, 1000);
+
+console.log("3: End App");
